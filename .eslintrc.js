920484482a76f31f4399718f1bc93d4efbf98db2
Zachary A Bensley <zabensley@gmail.com>
2020-08-18 17:01:47 -0700

    Start rTree.insert function.

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "globals": {
+        "describe": "readonly",
+        "beforeEach": "readonly",
+        "afterEach": "readonly",
+        "it": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+    },
+    "plugins": [
+        "@typescript-eslint",
+    ],
+    "rules": {
+        "no-unused-vars": 0,  // eslint flags imported types as unused variables.
+        "no-trailing-spaces": "error"
+    },
+};
